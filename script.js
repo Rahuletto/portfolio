d518d612787d052ad6e069fe451040ce48da966c
Rahuletto <rahulmarban@gmail.com>
2023-08-12 14:10:54 +0000

    some fixes

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 let header = document.querySelector('.header')
 window.addEventListener("scroll", () => {
   const currentScroll = window.pageYOffset;
-  console.log(currentScroll)
   if (currentScroll >= 100) {
     header.style.opacity = 1;
   } else {
