baf10c84b108213979d50c03a51dafb5e6fcfcc7
Rahul Marban <rahulmarban@gmail.com>
2023-12-31 10:07:31 +0000

    till projects

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,33 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import mdx from "@astrojs/mdx";
 
-// https://astro.build/config
-export default defineConfig({});
+import sitemap from "@astrojs/sitemap";
+
+import remarkToc from "remark-toc";
+import { rehypeAccessibleEmojis } from "rehype-accessible-emojis";
+import rehypeSlug from "rehype-slug";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypeToc from "rehype-toc";
+import { remarkReadingTime } from './plugins/read.mjs';
+
+export default defineConfig({
+  site: "https://marban.is-a.dev",
+  integrations: [mdx({ gfm: true }), sitemap(), react()],
+  markdown: {
+    syntaxHighlight: "shiki",
+    remarkPlugins: [[remarkToc, { heading: "contents" }], remarkReadingTime],
+    rehypePlugins: [
+      rehypeSlug,
+      [rehypeAutolinkHeadings, { behavior: "append" }],
+      [rehypeToc, { headings: ["h2", "h3"] }],
+      rehypeAccessibleEmojis
+    ],
+
+    shikiConfig: {
+      theme: "one-dark-pro",
+      wrap: true,
+    },
+    gfm: true,
+  },
+});
---
+++ src/pages/rss.xml.js
@@ -0,0 +1,15 @@
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
+
+export async function GET(context) {
+	const posts = await getCollection('blogs');
+	return rss({
+		title: "Rahuletto.",
+		description: "Building the future, pixel by pixel using a blend of code and imagination to design exceptional user experiences",
+		site: context.site,
+		items: posts.map((post) => ({
+			...post.data,
+			link: `/blogs/${post.slug}/`,
+		})),
+	});
+}
