478af3991119708aec179530cd477c9ec8baa756
Rahul Marban <rahulmarban@gmail.com>
2024-01-21 11:06:16 +0000

    feat: scroll animations

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
 import cloudflare from "@astrojs/cloudflare";
 
 // Remark
@@ -12,8 +11,16 @@ import rehypeSlug from "rehype-slug";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import rehypeToc from "rehype-toc";
 
+// Extensions
+import compress from "astro-compress";
+import preload from "astro-preload";
+import metaTags from "astro-meta-tags";
+
+import purgecss from "astro-purgecss";
+
+// https://astro.build/config
 export default defineConfig({
-  site: "https://marban.pages.dev",
+  site: "https://marban.is-a.dev",
   output: "hybrid",
   adapter: cloudflare({
     imageService: "cloudflare",
@@ -37,30 +44,67 @@ export default defineConfig({
     },
   },
   integrations: [
+    // compress(),
+    preload(),
+    sitemap(),
+    react(),
+    metaTags(),
+    // purgecss(),
     mdx({
       gfm: true,
       optimize: true,
-      remarkPlugins: [[remarkToc, { heading: "contents" }]],
+      remarkPlugins: [
+        [
+          remarkToc,
+          {
+            heading: "contents",
+          },
+        ],
+      ],
       rehypePlugins: [
         rehypeSlug,
-        [rehypeAutolinkHeadings, { behavior: "append" }],
-        [rehypeToc, { headings: ["h2", "h3"] }],
+        [
+          rehypeAutolinkHeadings,
+          {
+            behavior: "append",
+          },
+        ],
+        [
+          rehypeToc,
+          {
+            headings: ["h2", "h3"],
+          },
+        ],
         rehypeAccessibleEmojis,
       ],
     }),
-    sitemap(),
-    react(),
   ],
   markdown: {
     syntaxHighlight: "shiki",
-    remarkPlugins: [[remarkToc, { heading: "contents" }]],
+    remarkPlugins: [
+      [
+        remarkToc,
+        {
+          heading: "contents",
+        },
+      ],
+    ],
     rehypePlugins: [
       rehypeSlug,
-      [rehypeAutolinkHeadings, { behavior: "append" }],
-      [rehypeToc, { headings: ["h2", "h3"] }],
+      [
+        rehypeAutolinkHeadings,
+        {
+          behavior: "append",
+        },
+      ],
+      [
+        rehypeToc,
+        {
+          headings: ["h2", "h3"],
+        },
+      ],
       rehypeAccessibleEmojis,
     ],
-
     shikiConfig: {
       theme: "one-dark-pro",
       wrap: true,
