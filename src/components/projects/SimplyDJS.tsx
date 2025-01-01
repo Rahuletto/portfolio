d916fe0aa735fe0ba1eab255149ddbea99230645
Rahul Marban <rahulmarban@gmail.com>
2025-01-01 11:42:33 +0530

    style: gaps

--- src/components/projects/SimplyDJS.tsx
+++ src/components/projects/SimplyDJS.tsx
@@ -108,7 +108,7 @@ const SimplyDJS: React.FC = () => {
                 <div
                     ref={numRef}
                     className={tw(
-                        "flex items-center border-t border-color/40 w-full pt-3 md:!w-fit lg:!border-t-0 lg:border-l lg:!pt-0 lg:pl-6 justify-between",
+                        "flex gap-4 items-center border-t border-color/40 w-full pt-3 md:!w-fit lg:!border-t-0 lg:border-l lg:!pt-0 lg:pl-6 justify-between",
                     )}
                 >
                     <div className={tw("min-w-[100px]")}>
